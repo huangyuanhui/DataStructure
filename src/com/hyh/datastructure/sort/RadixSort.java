ee77db3c4a0d504a643ab5c29c99a8ae67ee7a57
huangyuanhui <2278181701@qq.com>
2019-12-18 22:00:21 +0800

    给定一个数组，求排序之后 相邻两数的最大差值 要求时间复杂度O(N) 且基于非比较的方法实现

--- src/com/hyh/datastructure/sort/RadixSort.java
+++ src/com/hyh/datastructure/sort/RadixSort.java
@@ -9,7 +9,7 @@ public class RadixSort {
         radixSort(arr);
         System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));*/
 
-        int[] arr = new int[80000];
+        /*int[] arr = new int[80000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 8000000);
         }
@@ -17,7 +17,10 @@ public class RadixSort {
         radixSort(arr);
         long end = System.currentTimeMillis();
         //O(n^2) 大概80ms
-        System.out.println("排序花费时间：" + (end - start) + "毫秒");
+        System.out.println("排序花费时间：" + (end - start) + "毫秒");*/
+
+        int[] arr = {80, 4, 155, 774, 10010, 3067, 666, 2};
+        System.out.println("maxGap(arr, arr.length) = " + maxGap(arr, arr.length));
     }
 
     private static void radixSort(int[] arr) {
@@ -69,4 +72,51 @@ public class RadixSort {
         }
         return max;
     }
+
+
+    //延伸：给定一个数组，求排序之后 相邻两数的最大差值 要求时间复杂度O(N) 且基于非比较的方法实现
+    private static int maxGap(int[] arr, int len) {
+        if (arr == null || len < 2) {
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            max = max > arr[i] ? max : arr[i];
+            min = min < arr[i] ? min : arr[i];
+        }
+        if (max == min) {
+            return 0;
+        }
+
+        int[] maxArr = new int[len + 1];
+        int[] minArr = new int[len + 1];
+        boolean[] hasNum = new boolean[len + 1];
+        minArr[0] = min;
+        hasNum[0] = true;
+
+        maxArr[maxArr.length - 1] = max;
+        hasNum[hasNum.length - 1] = true;
+
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            //抠边界
+            index = (len) * (arr[i] - min) / (max - min);
+            maxArr[index] = hasNum[index] ? Math.max(arr[i], maxArr[index]) : arr[i];
+            minArr[index] = hasNum[index] ? Math.min(arr[i], maxArr[index]) : arr[i];
+            hasNum[index] = true;
+        }
+
+        int res = 0;
+        int lastMax = maxArr[0];
+        for (int i = 0; i <= len; i++) {
+            if (hasNum[i]) {
+                res =  Math.max(minArr[i] - lastMax, res);
+                lastMax = maxArr[i];
+            }
+        }
+        return res;
+    }
+
 }
