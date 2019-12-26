ec5a9d30773e7ed2145aaffc6915ede4a97b3eac
huangyuanhui <2278181701@qq.com>
2019-12-26 19:08:07 +0800

    经典算法：KMP与Manacher代码实现

--- src/com/hyh/datastructure/advance/kmp/KMPDemo.java
+++ src/com/hyh/datastructure/advance/kmp/KMPDemo.java
@@ -1,4 +1,63 @@
 package com.hyh.datastructure.advance.kmp;
 
+//str2是否是str1的子串 如果是 请返回str2在str1开始的位置
 public class KMPDemo {
+    public static void main(String[] args) {
+        String str1 = "aabbccabcabcaaabbcc";
+        String str2 = "abcabca";
+        System.out.println("getIndexWithKMP(str1, str2) = " + getIndexWithKMP(str1, str2));
+    }
+
+    //KMP算法
+    private static int getIndexWithKMP(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() < 1 || str2.length() < 1) {
+            return -1;
+        }
+
+        char[] str1CharArr = str1.toCharArray();
+        char[] str2CharArr = str2.toCharArray();
+
+        int index1 = 0;
+        int index2 = 0;
+        int[] maxPrefixAndMaxSuffixCount = returnMaxPrefixAndMaxSuffix(str2CharArr);
+
+        while (index1 < str1CharArr.length && index2 < str2CharArr.length) {
+            if (str1CharArr[index1] == str2CharArr[index2]) {
+                index1++;
+                index2++;
+            } else if (maxPrefixAndMaxSuffixCount[index2] == -1) {
+                //index2回到了0位置
+                index1++;
+            } else {
+                //回到最长前缀的后一个位置 再去比较
+                index2 = maxPrefixAndMaxSuffixCount[index2];
+            }
+        }
+        return index2 == str2CharArr.length ? index1 - index2 : -1;
+    }
+
+    //获取一个字符串每个位置的字符的最长前缀和最长后缀
+    private static int[] returnMaxPrefixAndMaxSuffix(char[] strChars) {
+        if (strChars.length == 1) {
+            return new int[]{-1};
+        }
+        int[] maxPrefixAndMaxSuffixCount = new int[strChars.length];
+        maxPrefixAndMaxSuffixCount[0] = -1;
+        maxPrefixAndMaxSuffixCount[1] = 0;
+        //index索引前一个字符的的最长前缀最长后缀的下一个位置
+        int cn = 0;
+        int index = 2;
+        while (index < maxPrefixAndMaxSuffixCount.length) {
+            if (strChars[index - 1] == strChars[cn]) {
+                maxPrefixAndMaxSuffixCount[index++] = ++cn;
+            } else if (cn > 0) {
+                //cn变为cn位置字符的最长前缀与最长后缀
+                cn = maxPrefixAndMaxSuffixCount[cn];
+            } else {
+                //最长前缀与最长后缀为0
+                maxPrefixAndMaxSuffixCount[index++] = 0;
+            }
+        }
+        return maxPrefixAndMaxSuffixCount;
+    }
 }
--- src/com/hyh/datastructure/advance/manacher/ManacherDemo.java
+++ src/com/hyh/datastructure/advance/manacher/ManacherDemo.java
@@ -1,4 +1,63 @@
 package com.hyh.datastructure.advance.manacher;
 
+import java.util.Arrays;
+
+//str的最大回文子串的半径
 public class ManacherDemo {
+    public static void main(String[] args) {
+        String str1 = "abccbakkacbccbcacdd";
+        System.out.println("getMaxLcpsLengthWithManacher(str1) = " + getMaxLcpsLengthWithManacher(str1));
+    }
+
+    private static char[] manacherStr(String str1) {
+        char[] str1Chars = str1.toCharArray();
+        char[] str2Chars = new char[2 * str1Chars.length + 1];
+        int index = 0;
+        for (int i = 0; i < str2Chars.length; i++) {
+            str2Chars[i] = (i & 1) == 0 ? '#' : str1Chars[index++];
+        }
+        return str2Chars;
+    }
+
+    //用manacher算法获取字符串最大回文
+    private static int getMaxLcpsLengthWithManacher(String str) {
+        if (str == null || str.length() < 1) {
+            return 0;
+        }
+        char[] charArr = manacherStr(str);
+        //每个位置的回文半径
+        int[] pArr = new int[charArr.length];
+        //回文中心
+        int C = -1;
+        //回文右边界 初始回文右边界是-1
+        int R = -1;
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < charArr.length; i++) {
+            //2 * C - i 即 i 位置相对于回文中心C的 i'
+            //pArr[2 * C - i] 就是 i' 位置的回文半径
+            //所以pArr[i]表示的是i位置起码的回文半径
+            pArr[i] = R > i ? Math.min(pArr[2 * C - i], R - i) : 1;
+
+            while (i + pArr[i] < charArr.length && i - pArr[i] > -1) {
+                //暴力扩回文半径
+                if (charArr[i + pArr[i]] == charArr[i - pArr[i]]) {
+                    pArr[i]++;
+                } else {
+                    break;
+                }
+            }
+
+            //看是否需要更新回文右边界R以及回文中心C （i位置的回文右边界 与 R比较）
+            if (i + pArr[i] > R) {
+                R = i + pArr[i];
+                C = i;
+            }
+            //记录最大回文半径
+            max = Math.max(max, pArr[i]);
+        }
+        //这里就是回文直径啦（不要忘了我们加了虚轴#）
+        return max - 1;
+    }
 }
